6e885e853bbceac21c7d74986cb9ae6465e87ce1
Luís Costa <luiscosta205@gmail.com>
2023-02-21 17:42:35 +0000

    Remove need for exercise data in strategeis

--- server/strategies/outputTestCase.js
+++ server/strategies/outputTestCase.js
@@ -9,15 +9,12 @@ module.exports = {
     getFeedback: async(report, exercise, student_file) => {
         try {
             let feedback = "The correct results would be:\n"
-            let target = []
             const incorrect_tests = (report.tests.map((value, index) => { if (value.classify != "Accepted") return index })).filter((value) => { return value != undefined ? true : false });
-
             incorrect_tests.forEach(function(key) {
-                feedback += exercise.tests_contents_out[exercise.tests[key].id];
+                feedback += report.tests[key].expectedOutput
                 feedback += "\n";
-                target.push(key)
             });
-            return (new feedbackItem(feedback, 5, "INF", target, feedback_name))
+            return (new feedbackItem(feedback, 5, "INF", incorrect_tests, feedback_name))
         } catch (err) {
             console.log(err);
             return err
